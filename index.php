52fab69c7be2be34bb0337197e57696bd33c4d58
Bruno Marini <bruno.marini1@gmail.com>
2018-04-30 17:47:10 -0300

    code comments

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     if(isset($_SERVER['PATH_INFO'])){
+        //REST
         if (isset($_SERVER['HTTP_REST'])){
             include('Models/SQL_config.php');
             $command = 'UPDATE carro SET latitude = '.$_SERVER['HTTP_LATITUDE'].', longitude = '.$_SERVER['HTTP_LONGITUDE'].' WHERE placa = "' . $_SERVER['HTTP_PLACA'].'"; ';;
@@ -7,15 +8,17 @@
             $MySQL->close();
         }
         else{
+            //AJAX REQUEST
             if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
                 $file = 'Views' . $_SERVER['PATH_INFO'] . '.php';
                 include($file);  //Return file to AJAX request
             }
             else{
-                header("Location: " . '/');  //Redirect to index
+                header("Location: " . '/');  //Redirect to default
             }
         }
     }
+    //DEFAULT
     else{
         require_once('Views/index.php');  //Index View
     }
