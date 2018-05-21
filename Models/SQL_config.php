3eb6c7c2387bed2efde11dcd32c58d36614c58c5
Bruno Marini <bruno.marini1@gmail.com>
2018-05-21 13:43:18 -0300

    Heroku Database

--- Models/SQL_config.php
+++ Models/SQL_config.php
@@ -1,9 +1,9 @@
 <?php
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', 's3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 
     define('DB_NAME', 'irotas');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
+    define('DB_USER', 'tlol2vty7z359i8u');
+    define('DB_PASSWORD', 'd34644f8hjjdwsz2');
 
     define('DB_CHARSET', 'utf8');
 
